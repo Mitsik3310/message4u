bc3447a0158dfbbb1230d89fd0ccad531b05a371
Mitsik3310 <98687651+Mitsik3310@users.noreply.github.com>
2022-03-15 21:36:44 +0100

    final page

---
+++ sklep-main/scripts/checkbox.js
@@ -0,0 +1,10 @@
+function checkCheckbox() {
+  var checkBox = document.getElementById("myCheck");
+  var text = document.getElementById("text");
+
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
