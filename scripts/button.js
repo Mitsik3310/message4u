792214956b484d358fd45424e5aee81bec0866d7
Mitsik3310 <98687651+Mitsik3310@users.noreply.github.com>
2023-03-17 10:44:16 +0100

    Add files via upload

--- scripts/button.js
+++ scripts/button.js
@@ -1,5 +1,5 @@
 function runaway(id) {
-    id.style.top = Math.round(Math.random() * 250) - 100 + 'px';
-    id.style.left = Math.round(Math.random() * 250) - 200 + 'px';
-    id.style.right = Math.round(Math.random() * 200) - 200 + 'px';
+    id.style.top = Math.round(Math.random() * 500) - 100 + 'px';
+    id.style.left = Math.round(Math.random() * 550) - 200 + 'px';
+    id.style.right = Math.round(Math.random() * 500) - 200 + 'px';
 }
