3ebf0e79e59b5796c449d73cc42a5ab51c0998c1
Mitsik3310 <98687651+Mitsik3310@users.noreply.github.com>
2023-03-17 11:03:38 +0100

    Delete scripts directory

--- scripts/button.js
+++
@@ -1,5 +0,0 @@
-function runaway(id) {
-    id.style.top = Math.round(Math.random() * 500) - 100 + 'px';
-    id.style.left = Math.round(Math.random() * 550) - 200 + 'px';
-    id.style.right = Math.round(Math.random() * 500) - 200 + 'px';
-}
